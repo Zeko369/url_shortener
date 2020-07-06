e10c52f7761607b64edb7705dcc06bd89d0e902a
Fran Zekan <zekan.fran369@gmail.com>
2020-07-06 23:08:09 +0200

    Add chakra + change url to index

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,5 +1,6 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import QRCode from "qrcode.react"
+import { Button, Input } from "@chakra-ui/core"
 
 const Home: React.FC = () => {
   const download = () => {
@@ -18,27 +19,34 @@ const Home: React.FC = () => {
     }
   }
 
+  const [url, setUrl] = useState<string>("https://zekan.tk/t3")
+  const [panda, setPanda] = useState<boolean>(false)
+
   return (
     <div>
       <h1>Url shortener</h1>
+      <Input mb={3} value={url} onChange={(e) => setUrl(e.target.value)} />
+      <Button onClick={() => setPanda((p) => !p)}>Toggle panda</Button>
       <QRCode
         id="canvas"
-        value="https://zekan.tk/t3"
+        value={url}
         size={400}
-        renderAs="svg"
+        renderAs="canvas"
         level="H"
-        imageSettings={{
-          src:
-            "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/google/241/panda_1f43c.png",
-          x: null,
-          y: null,
-          height: 75,
-          width: 75,
-          excavate: true,
-        }}
+        imageSettings={
+          panda && {
+            src:
+              "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/google/241/panda_1f43c.png",
+            x: null,
+            y: null,
+            height: 75,
+            width: 75,
+            excavate: true,
+          }
+        }
       />
       <br />
-      <button onClick={download}>Download</button>
+      <Button onClick={download}>Download</Button>
     </div>
   )
 }
