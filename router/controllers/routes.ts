4a09d94daf1773568ff1a0457325a67caa903d90
Fran Zekan <zekan.fran369@gmail.com>
2020-07-11 00:06:34 +0200

    Refactor router

---
+++ router/controllers/routes.ts
@@ -0,0 +1,27 @@
+import { Request, Response } from "express"
+import db from "../../db"
+
+const routes = async (req: Request, res: Response) => {
+  let slug = req.params[0] as string
+
+  if (slug.startsWith("/")) {
+    slug = slug.slice(1)
+  }
+  if (slug.endsWith("/")) {
+    slug = slug.slice(0, -1)
+  }
+
+  if (slug.includes("/")) {
+    return res.send("This cant be nested, yet")
+  }
+
+  const link = await db.link.findOne({ where: { slug } })
+
+  if (link) {
+    res.redirect(link.url)
+  }
+
+  return res.send(`This slug doesn't exist`)
+}
+
+export default routes
