76a743c59ee4af5c521013c890b75016d622168a
Fran Zekan <zekan.fran369@gmail.com>
2020-07-11 19:10:42 +0200

    Add hello page for /

--- router/index.ts
+++ router/index.ts
@@ -8,6 +8,7 @@ import routes from "./controllers/routes"
 config({ path: join(__dirname, "../.env") })
 const app = express()
 
+app.get("/", (req, res) => res.json({ hello: "world" }))
 app.get("*", asyncMiddleware(routes))
 
 app.listen(3001, () => {
